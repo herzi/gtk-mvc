686f643e68b295d57c240464b3c362db109401b7
Sven Herzberg <herzi@gnome-de.org>
2010-02-16 17:06:55 +0100

    add arrow children to the button views
    * gtk-mvc-scroll-view.c: add arrow children to indicate the scroll
      direction

--- gtk-mvc-scroll-view.c
+++ gtk-mvc-scroll-view.c
@@ -53,11 +53,15 @@ gtk_scroll_view_init (GtkMvcScrollView* self)
 
   PRIV (self)->button_up = gtk_mvc_button_view_new ();
   gtk_mvc_view_set_model (PRIV (self)->button_up, gtk_mvc_button_model_new ());
+  gtk_mvc_button_view_set_child (GTK_MVC_BUTTON_VIEW (PRIV (self)->button_up),
+                                 gtk_mvc_arrow_view_new_with_model (gtk_mvc_arrow_model_new (GTK_ARROW_UP)));
   g_signal_connect (PRIV (self)->button_up, "queue-redraw",
                     G_CALLBACK (queue_redraw_from_child), self);
 
   PRIV (self)->button_down = gtk_mvc_button_view_new ();
   gtk_mvc_view_set_model (PRIV (self)->button_down, gtk_mvc_button_model_new ());
+  gtk_mvc_button_view_set_child (GTK_MVC_BUTTON_VIEW (PRIV (self)->button_up),
+                                 gtk_mvc_arrow_view_new_with_model (gtk_mvc_arrow_model_new (GTK_ARROW_UP)));
   g_signal_connect (PRIV (self)->button_down, "queue-redraw",
                     G_CALLBACK (queue_redraw_from_child), self);
 }
